0d1fb24bce7dd517a30b56cc42e237299e3ea2f1
ruuiisu <luis.gjokaj@gmail.com>
2023-09-07 14:41:27 +0200

    removed version check

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,13 @@ use std::io::BufReader;
 use xml::reader::{EventReader, XmlEvent};
 
 fn main() -> std::io::Result<()> {
-    let file = File::open("src/bomb.xml")?;
+    let file = File::open("src/test.xml")?;
     let file = BufReader::new(file); // Buffering is important for performance
 
     let parser = EventReader::new(file);
     let mut depth = 0;
     for e in parser {
         match e {
-            Ok(XmlEvent::StartDocument { version, encoding, standalone }) => {
-                if version.to_string() == "1.0" {
-                    println!("XML Version can not be 1.0!");
-                    break;
-                } 
-            }
             Ok(XmlEvent::StartElement { name, .. }) => {
                 println!("{:spaces$}+{name}", "", spaces = depth * 2);
                 depth += 1;
